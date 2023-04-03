cd4c6113c2781b73fcef58c206f2f938192d87db
laff1ck <125014665+laff1ck@users.noreply.github.com>
2023-04-03 14:28:51 +0300

    Add files via upload

---
+++ Prac7(kp)/code.h
@@ -0,0 +1,21 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <iostream>
+
+class type1 {
+public:
+    void Get_answer();
+    void show();
+};
+
+class type2 {
+private:
+    double A;
+public:
+    type2(double a1);
+    void Get_answer();
+    void show();
+};
+
+#endif
---
+++ Prac7(kp)/main.cpp
@@ -0,0 +1,13 @@
+#include "head.h"
+
+int main() {
+    type1 eq1;
+    eq1.show();
+    eq1.Get_answer();
+
+    type2 eq2(5.0);
+    eq2.show();
+    eq2.Get_answer();
+
+    return 0;
+}
