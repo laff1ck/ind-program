989829819167ab9682c1a29e71b083302ffeb9b1
laff1ck <125014665+laff1ck@users.noreply.github.com>
2023-04-03 14:06:28 +0300

    Add files via upload

---
+++ Prac7(kp)/Реализация первого метода.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class type1 {
+    public:
+    void Get_answer () {
+        cout << "Ответом является любое значение Х" << endl;
+    }
+    void show () {
+        cout << "Уравнение 0 = 0" << endl;
+    }
+};
