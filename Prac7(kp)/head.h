245401bf72c5438acbe5d71a13bd790b22791c19
laff1ck <125014665+laff1ck@users.noreply.github.com>
2023-04-03 14:20:23 +0300

    Add files via upload

---
+++ Prac7(kp)/head.h
@@ -0,0 +1,19 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+class type1 {
+    public:
+    void Get_answer ();
+    void show ();
+};
+
+class type2 {
+    private:
+    double A;
+    public:
+    type2 (double a1);
+    void Get_answer ();
+    void show ();
+};
+
+#endif
