3e328dc93e98082c8a925e10eb597c385b2ae748
laff1ck <125014665+laff1ck@users.noreply.github.com>
2023-04-09 21:22:10 +0300

    Add files via upload

---
+++ Prac8(kp)/code.h
@@ -0,0 +1,22 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+class type3 {
+private:
+    double C;
+public:
+    type3(double c1);
+    void Get_answer();
+    void show();
+};
+
+class type4 {
+private:
+    double A, C;
+public:
+    type4(double a1, double c1);
+    void Get_answer();
+    void show();
+};
+
+#endif // HEAD_H
---
+++ Prac8(kp)/main.cpp
@@ -0,0 +1,13 @@
+#include "head.h"
+
+int main() {
+    type3 eq3(7.0);
+    eq3.show();
+    eq3.Get_answer();
+
+    type4 eq4(2.0, 5.0);
+    eq4.show();
+    eq4.Get_answer();
+
+    return 0;
+}
